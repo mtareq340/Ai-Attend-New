d1f7ffa69f6d57a1ddf1336cde6622fe7f73ded1
mohamedmagdy20 <mohamedmagdymohamed982@gmail.com>
2022-07-05 08:49:00 +0200

    login histroy tabel

--- __src/ai-attend_V11.sql
+++ __src/ai-attend_V11.sql
@@ -1127,6 +1127,17 @@ ALTER TABLE `work_appointments`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
+CREATE TABLE `login_histories` (
+  `id` bigint(20) NOT NULL,
+  `user_id` bigint(20) NOT NULL,
+  `ip` varchar(191) NOT NULL,
+  `datetime` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `details` varchar(191) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
