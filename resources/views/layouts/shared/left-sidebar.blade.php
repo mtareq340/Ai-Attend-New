63ad6a08b9469d6df708cbf5054f7ac04946c2c4
mtareq340 <mtareq340@gmail.com>
2022-07-16 10:08:55 +0200

    tarek

--- resources/views/layouts/shared/left-sidebar.blade.php
+++ resources/views/layouts/shared/left-sidebar.blade.php
@@ -336,21 +336,18 @@
                 @endcan
                 {{-- end of appointment --}}
 
-                {{-- start of Management Plans  --}}
-                {{-- @can('show_assign_appointments') --}}
-                    <li>
-                        {{-- <a href="#sidebarassign" data-toggle="">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                                class="feather feather-clipboard icon-dual">
-                                <path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"></path>
-                                <rect x="8" y="2" width="8" height="4" rx="1" ry="1"></rect>
-                            </svg>
-                        </a> --}}
-                        <a href="#"> Management Plans</a>
-                        {{-- <p>Management Plans</p>                        --}}
-                    </li>
-                {{-- @endcan --}}
+         
+
+                @can('show_assign_appointments')
+                <li>
+                    <a href="{{ route('assign_appointment.index') }}">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-clipboard icon-dual"><path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"></path><rect x="8" y="2" width="8" height="4" rx="1" ry="1"></rect></svg>
+                        <span>Management Plans</span>
+                        <span class=""></span>
+                    </a>
+                
+                </li>
+                @endcan
 
                 @can('show_extra_time')
                 <li>
